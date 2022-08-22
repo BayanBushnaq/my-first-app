1516f054986d21aed7d451f22e5d1bf5cdcd780e
BayanBushnaq <bayanmohdali@yahoo.com>
2022-08-22 22:28:15 +0300

    lab2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import Header from './Components/Header';
-import Main from './Components/Main';
-import Footer from './Components/Footer';
-// import HornedBeast from './Components/HornedBeast';
+import Parent from './CompLab2/Parent';
+
 
 class App extends React.Component{
   render(){
     return (
-    <div>
-      <Header/>
-      <Main/>
-      <Footer/>
+    
+      <div className="cards"><Parent/></div>
       
-    </div>)
+     
+    )
   }
 }
 
@@ -20,28 +17,5 @@ export default App;
 
 
 
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 
